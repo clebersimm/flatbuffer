a5bada8732c53bc53ce77cf38ebee5ec3944c951
Cleber Simm <clebersimm@gmail.com>
2018-03-27 13:51:03 -0300

    Try to create a test player and return the binary. Not working yet

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	player "teste"
+
+	"github.com/gin-gonic/gin"
+	flatbuffers "github.com/google/flatbuffers/go"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/Player", func(c *gin.Context) {
+		builder := flatbuffers.NewBuilder(0)
+		player.PlayerStart(builder)
+		playerName := builder.CreateString("test")
+		playerTitle := builder.CreateString("the destroyer")
+		player.PlayerAddName(builder, playerName)
+		player.PlayerAddTitle(builder, playerTitle)
+		currentPlayer := player.PlayerEnd(builder)
+		builder.Finish(currentPlayer)
+		c.Writer.Header().Set("Content-type", "application/octet-stream")
+		c.Writer.WriteHeader(200)
+		c.Writer.Write(builder.Bytes)
 		//TODO add response with flatbuffer
 	})
-	r.Run("4040")
+	r.Run(":8040")
 }
